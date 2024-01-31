2e082750ddbbe1a610fac445339e6da6e80503f4
jaejung <santoki389@gmial.com>
2024-01-31 14:30:52 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffer/test/TestApplication.java
@@ -0,0 +1,26 @@
+package com.ohgiraffer.test;
+
+public class TestApplication {
+    // 수업목표
+    // 여기는 주석입니다.
+    /*여기는
+    * 여러 줄
+    * 주석 입니다
+    * 김재중
+    * 바보
+    * */
+    // ()
+
+    /*
+    * ctrl - space : 자동완성 기능
+    * ctrl - d
+    * */
+
+    // 복사예시
+    // 복사예시
+    // 복사예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+    }
+}
